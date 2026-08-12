095f2f765cb4369ca54b0dfe813b18c552585d29
kristilb1999 <kl03boar@siena.edu>
2026-08-11 20:31:22 -0400

    Create initial Spring Boot application

---
+++ src/main/java/com/wellnessproject/WellnessprojectApplication.java
@@ -0,0 +1,13 @@
+package com.wellnessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellnessproject/WellnessprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellnessproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
